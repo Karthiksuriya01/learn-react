852922db5a11cac5baf18f0f7684959e6c190031
karthiksuriya01 <karthiksuriy01@gmail.com>
2025-01-07 23:50:12 +0530

    Updated

--- 13-React+shadcnui/src/App.jsx
+++ 13-React+shadcnui/src/App.jsx
@@ -1,37 +1,81 @@
+// // import React from 'react';
+// import { Button } from './components/ui/button';
+// import {
+//   AlertDialog,
+//   AlertDialogAction,
+//   AlertDialogCancel,
+//   AlertDialogContent,
+//   AlertDialogDescription,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogTitle,
+//   AlertDialogTrigger,
+// } from "@/components/ui/alert-dialog"
+
+// const App = () => {
+//   return (
+//     <div>
+//     <Button><AlertDialog>
+//   <AlertDialogTrigger>Open</AlertDialogTrigger>
+//   <AlertDialogContent>
+//     <AlertDialogHeader>
+//       <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//       <AlertDialogDescription>
+//         This action cannot be undone. This will permanently delete your account
+//         and remove your data from our servers.
+//       </AlertDialogDescription>
+//     </AlertDialogHeader>
+//     <AlertDialogFooter>
+//       <AlertDialogCancel>Cancel</AlertDialogCancel>
+//       <AlertDialogAction>Continue</AlertDialogAction>
+//     </AlertDialogFooter>
+//   </AlertDialogContent>
+// </AlertDialog>
+// </Button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 // import React from 'react';
-import { Button } from './components/ui/button';
+
+import { Card, CardContent } from "@/components/ui/card"
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 const App = () => {
   return (
     <div>
-    <Button><AlertDialog>
-  <AlertDialogTrigger>Open</AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-      <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-    <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction>Continue</AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>
-</Button>
+      <Carousel
+      opts={{
+        align: "start",
+        loop:true,
+      }}
+    
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
     </div>
   );
 }
